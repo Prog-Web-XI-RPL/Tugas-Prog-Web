4ef8220332de8b7419eb3528e176984610d8f214
Khenjy <71621764+Khenjy@users.noreply.github.com>
2021-03-08 17:12:33 +0700

    Update elor.php

--- elor.php
+++ elor.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html>
 <head>
   <meta charset="utf-8">
   <title>Belajar Javascript</title>
 </head>
 <body>
-  <h1>ELLOR</h1>
+  <h1 style="color:red;" align:center>ELLOR</h1>
+</body>
+</html>
